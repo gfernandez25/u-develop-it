445c2263b0b7e19f73a9bedc3fe58dd24d0ed38b
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-09-03 00:21:47 -0400

    added voter routes

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+//connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'mysql123',
+    database: 'election'
+});
+
+module.exports = db;
+exports = db;
